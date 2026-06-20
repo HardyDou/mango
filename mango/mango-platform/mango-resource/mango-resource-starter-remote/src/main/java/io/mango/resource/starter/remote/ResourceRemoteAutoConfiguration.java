e9c8a251194c7edafcada8bcbf4f4be96b72f68f
hardyDou <doouxiaoyu@163.com>
2026-06-20 08:17:55 +0800

    fix: map capability module service targets
    Refs #186

--- mango/mango-platform/mango-resource/mango-resource-starter-remote/src/main/java/io/mango/resource/starter/remote/ResourceRemoteAutoConfiguration.java
+++ mango/mango-platform/mango-resource/mango-resource-starter-remote/src/main/java/io/mango/resource/starter/remote/ResourceRemoteAutoConfiguration.java
@@ -1,9 +1,11 @@
 package io.mango.resource.starter.remote;
 
 import io.mango.infra.feign.starter.ModuleTargetResolver;
+import io.mango.resource.api.ResourceHandler;
 import io.mango.resource.api.ResourceTargetDispatcher;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
  * 资源注册中心远程自动配置。
  */
 @Configuration
-@EnableFeignClients(basePackages = "io.mango.resource.starter.remote")
 public class ResourceRemoteAutoConfiguration {
 
     @Bean
@@ -19,5 +20,12 @@ public class ResourceRemoteAutoConfiguration {
     public ResourceTargetDispatcher resourceTargetDispatcher(ModuleTargetResolver moduleTargetResolver,
                                                              ResourceTargetFeignClient targetFeignClient) {
         return new RemoteResourceTargetDispatcher(moduleTargetResolver, targetFeignClient);
+    }
+
+    @Bean
+    @ConditionalOnBean(ResourceHandler.class)
+    @ConditionalOnMissingBean
+    public ResourceTargetController resourceTargetController(ObjectProvider<ResourceHandler> handlers) {
+        return new ResourceTargetController(handlers);
     }
 }
